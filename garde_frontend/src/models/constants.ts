4c5fda59a028ef96b01f3e3002bed53b1e133709
khalil <mohamedkhalilkt@gmail.com>
2024-04-25 14:33:01 +0100

    ad

--- garde_frontend/src/models/constants.ts
+++ garde_frontend/src/models/constants.ts
@@ -1,6 +1,7 @@
 // export const rootUrl = "http://127.0.0.1:8001/";
 // export const rootUrl = "http://backend:8001";
-export const rootUrl = "http://localhost/api/";
+// export const rootUrl = "http://localhost/api/";
+export const rootUrl = "http://162.0.239.121/api/";
 
 export interface PaginatedData<T> {
   count: number;
